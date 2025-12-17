fa5f4e00a9608626fbb77956eaed5777a280e794
toxicovi <69158460+toxicovi@users.noreply.github.com>
2025-12-17 19:48:52 +0800

    Update model.py

--- litefndpp/model.py
+++ litefndpp/model.py
@@ -1,26 +1,27 @@
-from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.naive_bayes import MultinomialNB
 import numpy as np
 
-class LiteFNDpp:
+class LiteFNDppModel:
+    """
+    Basic LiteFND++ ensemble: Logistic Regression + Naive Bayes
+    """
     def __init__(self):
-        self.vectorizer = TfidfVectorizer(ngram_range=(1,4), max_features=10000)
         self.lr = LogisticRegression(C=1.8, max_iter=1000)
         self.nb = MultinomialNB(alpha=0.03)
+        self.lr_weight = 0.65
+        self.nb_weight = 0.35
+        
+    def fit(self, X, y):
+        self.lr.fit(X, y)
+        self.nb.fit(X, y)
+        
+    def predict_proba(self, X):
+        lr_proba = self.lr.predict_proba(X)
+        nb_proba = self.nb.predict_proba(X)
+        return self.lr_weight * lr_proba + self.nb_weight * nb_proba
     
-    def fit(self, texts, labels):
-        X = self.vectorizer.fit_transform(texts)
-        self.lr.fit(X, labels)
-        self.nb.fit(X, labels)
-    
-    def predict_proba(self, texts):
-        X = self.vectorizer.transform(texts)
-        p_lr = self.lr.predict_proba(X)
-        p_nb = self.nb.predict_proba(X)
-        # Weighted soft voting
-        return 0.65*p_lr + 0.35*p_nb
-    
-    def predict(self, texts):
-        p = self.predict_proba(texts)
-        return np.argmax(p, axis=1)
+    def predict(self, X):
+        proba = self.predict_proba(X)
+        return (proba[:,1] > 0.5).astype(int)
+
